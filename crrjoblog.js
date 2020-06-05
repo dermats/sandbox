3bf209e90228a73880e4b487717ae54314604351
Mathias <29299212+dermats@users.noreply.github.com>
2020-06-05 17:57:49 +0200

    Update crrjoblog.js

--- crrjoblog.js
+++ crrjoblog.js
@@ -36,7 +36,7 @@ var tableId = document.querySelectorAll("[id^=__table]")[0].id;
 var oTable =  sap.ui.getCore().byId(tableId);
 var data = oTable.getData();
 var details = []
-var detailsCsv = [['Id', 'Status', 'SO', 'SoItems', 'ItemCount', 'ExecTime', 'LastUpdateDate', 'LastUpdateTime', 'TimeInQueue', 'failedStep', 'Trigger', 'BotStatus']];
+var detailsCsv = [['Id', 'Status', 'SO', 'SoItems', 'ItemCount', 'ExecTime', 'LastUpdateDate', 'LastUpdateTime', 'TimeInQueue', 'failedStep', 'Trigger', 'BotStatus', 'TotalItems']];
 for (var i in data.data) {
      var runUid = data.data[i].runUid; 
      document.title = 'Get ' + runUid;
@@ -60,7 +60,8 @@ for (var i in data.data) {
 			result.runStatusResponse.length > 2 ? Math.ceil((Date.parse(result.runStatusResponse[1].updated) - Date.parse(result.runStatusResponse[0].updated)) / 1000) : 'n/a',
 			result.jobRunResponse.runStatus == 'failed' ? result.runStatusResponse[2].detail : '',
 			result.jobRunResponse.triggerName,
-			result.runLogResponse[0].content.E5ZData.Bot_Status
+			result.runLogResponse[0].content.E5ZData.Bot_Status,
+		 	result.runLogResponse[0].content.SO_Data.Total_Items
 		 ]);    
      }});
      document.title='Done';
